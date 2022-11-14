9239738258734ecec1855e83a6d31ea0279d542a
Aristide Tchoukoualeu <aristide.tchouanga@gmail.com>
2022-11-14 11:10:00 +0100

    fixing bug

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,10 +1,10 @@
 export function formatDate(date?: string) {
   if (!date) return ""
 
-  const dateObject = new Date()
+  const dateObject = new Date(date)
 
-  const day = dateObject.getDate()
-  const month = dateObject.getMonth()
+  const day = ("0" + dateObject.getDate()).slice(-2)
+  const month = ("0" + dateObject.getMonth()).slice(-2)
   const year = dateObject.getFullYear()
 
   return `${day}.${month}.${year}`
